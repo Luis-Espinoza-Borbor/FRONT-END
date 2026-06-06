bd84700c4d5858e50c7952d32715ec765caef918
Luis-Espinoza-Borbor <luis.espinoza@tecnologicoliceocristiano.edu.ec>
2026-06-06 09:04:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ fetch('http://cine.runasp.net/api/Actores').then(respuesta => respuesta.json()).
 fetch('http://cine.runasp.net/api/Actores/13').then(respuesta => respuesta.json()).then(informacion => console.log(informacion)).catch(error => console.log(error));
 
 const actor = {
-    nombre: "Vin Diesel",
-    imagen: "url.jpg",
-    fechaNacimiento: "2026-06-T14:37:07.2902",
-    nacionalidad: "ecuatoriano" 
-}
+    nombre: "alfredo ordoñez",
+    imagen: "foto.jpg",
+    fechaNacimiento: "1996-08-26T00:00:00",
+    nacionalidad: "ecuador"
+  }
 
 fetch('http://cine.runasp.net/api/Actores', {
     method : 'POST',
     headers : {
-        "Content - Type": "application/json"
+        "Content-Type": "application/json"
     },
     body: JSON.stringify(actor)
 }).then(respuesta => respuesta.json()).then(informacion => console.log(informacion)).catch(error => console.log(error));
